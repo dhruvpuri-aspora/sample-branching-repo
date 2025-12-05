409b9210849abcd78dd79a65bcdf370d44104d87
dhruvpuri-aspora <dhruv.puri@aspora.com>
2025-12-05 16:51:48 +0530

    refactor: enhance test feature with v2 functionality

--- test-feature.js
+++ test-feature.js
@@ -1,3 +1,8 @@
 // Testing new feature directly on stage-env-copy
 console.log('Testing feature deployment');
 
+// Updated with more test functionality
+export function testFeature() {
+  return 'test-feature-v2';
+}
+
